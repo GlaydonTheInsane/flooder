58e9ab492df2ac89a4d65da18052bbe5b2c657ff
GlaydoN <46568926+GlaydonTheInsane@users.noreply.github.com>
2020-02-07 22:37:22 +0300

    Update Flooder_Fear_Fallen.lua

--- Flooder_Fear_Fallen.lua
+++ Flooder_Fear_Fallen.lua
@@ -7,7 +7,7 @@ require("lib.moonloader")
 
 function main()
 while not isSampAvailable() do wait(0) end
-sampAddChatMessage("{32CD32}Flooder {20B2AA}| Автор: Fear Fallen", -1)
+sampAddChatMessage("{32CD32}Flooder {20B2AA}| РђРІС‚РѕСЂ: Fear Fallen", -1)
 sampAddChatMessage("{32CD32}Flooder {20B2AA}| /helpflood!", -1)
 sampRegisterChatCommand("flbot", flooder)
 sampRegisterChatCommand("fldelay", fdelay)
@@ -15,18 +15,18 @@ sampRegisterChatCommand("helpflood", helpflood)
 while true do
 	wait(delay)
 	if faqrus == true then
-		sampAddChatMessage("{979797}Настройки флуд бота: {ff0000}/fldelay - {866e00}Задержка флуда | {ff0000}/flbot - {866e00}Бот", 0xFFFFFF)
+		sampAddChatMessage("{979797}ГЌГ Г±ГІГ°Г®Г©ГЄГЁ ГґГ«ГіГ¤ ГЎГ®ГІГ : {ff0000}/fldelay - {866e00}Г‡Г Г¤ГҐГ°Г¦ГЄГ  ГґГ«ГіГ¤Г  | {ff0000}/flbot - {866e00}ГЃГ®ГІ", 0xFFFFFF)
 		faqrus = false
 	end
 if fbdelay == true then
-	sampAddChatMessage(string.format("{979797}Были {ff0000}изменены{979797} настройки задержки {218734}[Delay: %s]", delay), 0xFFFFFFF)
+	sampAddChatMessage(string.format("{979797}ГЃГ»Г«ГЁ {ff0000}ГЁГ§Г¬ГҐГ­ГҐГ­Г»{979797} Г­Г Г±ГІГ°Г®Г©ГЄГЁ Г§Г Г¤ГҐГ°Г¦ГЄГЁ {218734}[Delay: %s]", delay), 0xFFFFFFF)
 	fbdelay = false
 end
 if fbot == true then
 	if delay > 0 then
 		sampSendChat(string.format("%s", flood))
 	else
-		sampAddChatMessage(string.format("Задержка меньше или равна 0. Поменяйте её через команду /fldelay"))
+		sampAddChatMessage(string.format("Г‡Г Г¤ГҐГ°Г¦ГЄГ  Г¬ГҐГ­ГјГёГҐ ГЁГ«ГЁ Г°Г ГўГ­Г  0. ГЏГ®Г¬ГҐГ­ГїГ©ГІГҐ ГҐВё Г·ГҐГ°ГҐГ§ ГЄГ®Г¬Г Г­Г¤Гі /fldelay"))
 	end
 end
 	end
